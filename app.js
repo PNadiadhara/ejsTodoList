05118f7a4678756c98c00cda42f7a7e4a0c3fac8
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2021-06-15 21:20:13 -0400

    ejs tags

--- app.js
+++ app.js
@@ -12,13 +12,29 @@ app.get("/", function(req, res){
 
     var today = new Date();
     var currentDay = today.getDay();
+    var weekday=new Array(7);
+    weekday[0]="Monday";
+    weekday[1]="Tuesday";
+    weekday[2]="Wednesday";
+    weekday[3]="Thursday";
+    weekday[4]="Friday";
+    weekday[5]="Saturday";
+    weekday[6]="Sunday";
+
+    var day = "";
 // res.send is final peice of info being sent, vs res.write
     if (currentDay === 6 || currentDay === 0 ) {
-        res.write("<h1>Happy Weekend</h1>")
+        
+        day = weekday[currentDay]
+        
+        
     } else {
-        res.sendFile(__dirname + "/index.html")
+        
+        day = weekday[currentDay]
         
     }
+    // list referes to list from /views/list.ejs
+    res.render("list", {kindOfDay: day})
 });
 
 app.listen(3000, function(){
